6a8c75ffff18987cc7836363148cc104aab01b95
Jackeagle <jackeagle102@gmail.com>
2020-06-25 10:52:51 +0200

    Settings: Switch to string overlay for maintainer string
    - Define following overlay in your device tree
        <string name="octavi_maintainer"></string>
    
    Change-Id: Ia637b532ec6a153343caa1fe1e7d86e3af59dea6
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Signed-off-by: Rahul <rahul731999@gmail.com>

--- src/com/android/settings/deviceinfo/octavi/OctaviMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/octavi/OctaviMaintainerPreferenceController.java
@@ -17,7 +17,7 @@
 package com.android.settings.deviceinfo.octavi;
 
 import android.content.Context;
-import android.os.SystemProperties;
+import android.content.res.Resources;
 import android.text.TextUtils;
 
 import androidx.preference.Preference;
@@ -28,7 +28,6 @@ import com.android.settings.core.BasePreferenceController;
 public class OctaviMaintainerPreferenceController extends BasePreferenceController {
 
     private static final String TAG = "OctaviMaintainerPreferenceController";
-    private static final String ROM_PROPERTY = "ro.octavi.maintainer";
 
     public OctaviMaintainerPreferenceController(Context context, String key) {
         super(context, key);
@@ -39,8 +38,7 @@ public class OctaviMaintainerPreferenceController extends BasePreferenceControll
     }
 
     public CharSequence getSummary() {
-        String rom = SystemProperties.get(ROM_PROPERTY,
-                this.mContext.getString(R.string.device_info_default));
-        return rom;
+        String maintainer = mContext.getResources().getString(R.string.octavi_maintainer);
+        return maintainer;
     }
 }
