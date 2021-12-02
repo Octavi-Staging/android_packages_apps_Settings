338d4000e5950a58323b44df4a9edb28d7092645
Pranav Vashi <neobuddy89@gmail.com>
2021-12-03 00:13:42 +0530

    NetworkProviderSettings: Prevent Wifi Manager NPE
    Log:
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: FATAL EXCEPTION: main
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: Process: com.android.settings, PID: 2508
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'boolean android.net.wifi.WifiManager.isAutoWakeupEnabled()' on a null object reference
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at com.android.settings.network.NetworkProviderSettings.isWifiWakeupEnabled(NetworkProviderSettings.java:1040)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at com.android.settings.network.NetworkProviderSettings.setAdditionalSettingsSummaries(NetworkProviderSettings.java:1004)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at com.android.settings.network.NetworkProviderSettings.updateWifiEntryPreferences(NetworkProviderSettings.java:932)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at com.android.settings.network.NetworkProviderSettings.onWifiStateChanged(NetworkProviderSettings.java:751)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at com.android.settings.network.NetworkProviderSettings.lambda$onInternetTypeChanged$4(NetworkProviderSettings.java:733)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at com.android.settings.network.NetworkProviderSettings.$r8$lambda$3_7zXKK2wdP9OmcsOQoBAsG6DTg(Unknown Source:0)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at com.android.settings.network.NetworkProviderSettings$$ExternalSyntheticLambda4.run(Unknown Source:2)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at android.os.Handler.handleCallback(Handler.java:938)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:99)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:201)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:288)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:7839)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:548)
    12-02 23:56:08.899  2508  2508 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:1003)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Sagarrokade006 <Sagarrokade006@gmail.com>
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- src/com/android/settings/network/NetworkProviderSettings.java
+++ src/com/android/settings/network/NetworkProviderSettings.java
@@ -549,7 +549,7 @@ public class NetworkProviderSettings extends RestrictedSettingsFragment
             if (resultCode == Activity.RESULT_OK) {
                 final WifiConfiguration wifiConfiguration = data.getParcelableExtra(
                         ConfigureWifiEntryFragment.NETWORK_CONFIG_KEY);
-                if (wifiConfiguration != null) {
+                if (mWifiManager != null && wifiConfiguration != null) {
                     mWifiManager.connect(wifiConfiguration,
                             new WifiConnectActionListener());
                 }
@@ -1156,8 +1156,11 @@ public class NetworkProviderSettings extends RestrictedSettingsFragment
         final Context context = getContext();
         final PowerManager powerManager = context.getSystemService(PowerManager.class);
         final ContentResolver contentResolver = context.getContentResolver();
-        return hasWifiManager()
-                && mWifiManager.isAutoWakeupEnabled()
+        if (mWifiManager == null && getActivity() != null)
+            mWifiManager = getActivity().getSystemService(WifiManager.class);
+        if (mWifiManager == null)
+            return false;
+        return mWifiManager.isAutoWakeupEnabled()
                 && mWifiManager.isScanAlwaysAvailable()
                 && Settings.Global.getInt(contentResolver,
                 Settings.Global.AIRPLANE_MODE_ON, 0) == 0
@@ -1178,7 +1181,7 @@ public class NetworkProviderSettings extends RestrictedSettingsFragment
     private void handleAddNetworkSubmitEvent(Intent data) {
         final WifiConfiguration wifiConfiguration = data.getParcelableExtra(
                 AddNetworkFragment.WIFI_CONFIG_KEY);
-        if (wifiConfiguration != null && hasWifiManager()) {
+        if (mWifiManager != null && wifiConfiguration != null) {
             mWifiManager.save(wifiConfiguration, mSaveListener);
         }
     }
@@ -1225,7 +1228,7 @@ public class NetworkProviderSettings extends RestrictedSettingsFragment
             if (config == null) {
                 Toast.makeText(getContext(), R.string.wifi_failed_save_message,
                         Toast.LENGTH_SHORT).show();
-            } else {
+            } else if (mWifiManager != null) {
                 mWifiManager.save(config, mSaveListener);
             }
         } else if (dialogMode == WifiConfigUiBase2.MODE_CONNECT
@@ -1233,7 +1236,7 @@ public class NetworkProviderSettings extends RestrictedSettingsFragment
             if (config == null) {
                 connect(wifiEntry, false /* editIfNoConfig */,
                         false /* fullScreenEdit*/);
-            } else {
+            } else if (mWifiManager != null) {
                 mWifiManager.connect(config, new WifiConnectActionListener());
             }
         }
